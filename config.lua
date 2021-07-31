d47eb4d4bfe39295f046c6dce55445df5c5590aa
Quinten-Gelderland <75490815+Quinten-Gelderland@users.noreply.github.com>
2021-07-31 16:29:39 +0200

    Github Uplouad

---
+++ config.lua
@@ -0,0 +1,22 @@
+HD={} 
+
+HD.tijd = 5 -- 5 is 5 minuten je kunt het verzetten naar wat je wilt, dit zou zijn hoe lang het elk bericht zou verzenden
+
+HD.naam = 'Server naam' -- Je server naam hier
+
+HD.achter = ''
+
+--Bericht Config
+HD.padding = '0.3'
+HD.margin = '0.1'
+HD.border = '3'
+HD.foto = "fas fa-bullhorn"
+HD.kleur = 'rgba(255, 149, 0, 0.4)'
+
+HD.berichten = {
+    'Typ hier je eigen bericht',      -- verander dit naar je bericht die je zelf wil 
+    'Typ hier je eigen bericht',
+    'Typ hier je eigen bericht',
+    'Typ hier je eigen bericht',  
+    'Typ hier je eigen bericht',
+}
